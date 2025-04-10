014b97a5cf479ce9614b0d29146ca8cbb633a0ae
Eddie4k-code <edwardoc@buffalo.edu>
2025-04-09 21:04:57 -0400

    Add mvn proj

---
+++ duplicate-message-detector-kafka-connect-custom-smt/src/main/java/io/github/eddie4k/App.java
@@ -0,0 +1,13 @@
+package io.github.eddie4k;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
